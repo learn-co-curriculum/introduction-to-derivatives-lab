251c92bae3e2543ed34dfbef4d53749a41d41b9a
Jeffrey Katz <JeffreyEricKatz@gmail.com>
2018-04-23 11:21:28 -0400

    change names of functions, change introduction

--- graph.py
+++ graph.py
@@ -24,15 +24,15 @@ def m_b_trace(m, b, x_values, mode = 'line', name = 'line function'):
     values = m_b_data(m, b, x_values)
     values.update({'mode': mode, 'name': name})
     return values
-    
+
 def plot(traces, layout = {}):
     if not isinstance(traces, list): raise TypeError('first argument must be a list.  Instead is', traces)
     plotly.offline.iplot({'data': traces, 'layout': layout})
 
-def build_layout(x_range = None, y_range = None, options = {}):
+def build_layout(x_axis = None, y_axis = None, options = {}):
     layout = {}
-    if isinstance(x_range, list): layout.update({'xaxis': {'range': x_range}})
-    if isinstance(y_range, list): layout.update({'yaxis': {'range': y_range}})
+    if isinstance(x_axis, dict): layout.update({'xaxis': x_axis})
+    if isinstance(y_axis, dict): layout.update({'yaxis': y_axis})
     layout.update(options)
     return layout
 
