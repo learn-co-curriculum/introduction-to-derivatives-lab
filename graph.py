7cc04a72e254b3121ec8abad2580ea3867b566d0
Jeffrey Katz <JeffreyEricKatz@gmail.com>
2018-04-23 13:01:02 -0400

    add side by side plots

--- graph.py
+++ graph.py
@@ -1,11 +1,15 @@
 import plotly
 from plotly.offline import iplot, init_notebook_mode
+from plotly import tools
 init_notebook_mode(connected=True)
 
 def trace(data, mode = 'markers', name="data"):
     x_values = list(map(lambda point: point['x'],data))
     y_values = list(map(lambda point: point['y'],data))
     return {'x': x_values, 'y': y_values, 'mode': mode, 'name': name}
+
+def plot_figure(figure):
+    plotly.offline.iplot(figure)
 
 def line_function_trace(line_function, x_values, mode = 'line', name = 'line function'):
     values = line_function_data(line_function, x_values)
@@ -36,6 +40,21 @@ def build_layout(x_axis = None, y_axis = None, options = {}):
     layout.update(options)
     return layout
 
+
+def make_subplots(one_one_traces = [], one_two_traces = [], two_one_traces = [], two_two_traces = []):
+    if two_one_traces or two_two_traces:
+        fig = tools.make_subplots(rows=2, cols=2)
+    else:
+        fig = tools.make_subplots(rows=1, cols=2)
+    for trace in one_one_traces:
+        fig.append_trace(trace, 1, 1)
+    for trace in one_two_traces:
+        fig.append_trace(trace, 1, 2)
+    for trace in two_one_traces:
+        fig.append_trace(trace, 1, 1)
+    for trace in two_two_traces:
+        fig.append_trace(trace, 1, 2)
+    return fig
 
 def trace_values(x_values, y_values, mode = 'markers', name="data", text = []):
     return {'x': x_values, 'y': y_values, 'mode': mode, 'name': name, 'text': text}
